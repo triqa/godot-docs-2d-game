5e7a416b89f11baea7b9f6fb8c1f11dac23290b6
zira <66490318+triqa@users.noreply.github.com>
2026-04-01 17:14:58 +0100

    add details to README

--- main.gd
+++ main.gd
@@ -1,5 +1,8 @@
 extends Node
 
+# can now choose the mob scene want to instance
+@export var mob_scene: PackedScene
+var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +12,24 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+func _on_mob_timer_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_score_timer_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_start_timer_timeout() -> void:
+	pass # Replace with function body.
